1766013d10517fc3319518a4917b25bbe0b46b73
kristakernodle <kristakernodle@gmail.com>
2019-06-05 14:35:00 -0400

    Completed create project through frame extraction

--- createProj_DLC.py
+++ createProj_DLC.py
@@ -4,18 +4,16 @@ Created on Wed Jun  5 11:54:18 2019
 
 @author: kkrista
 """
-
-import os
 import setDLCFunc
 import deeplabcut
 
 ## Define User Input Variables
 
 # Project variables
-projName = 'left_groomingIso'
+projName = 'left_grooming'
 experimenter = 'Krista'
 projDir = '/home/kkrista/Documents/Grooming/Krista/'
-numRandVids = 20 # number of videos to train from
+numRandVids = 100 # number of videos to train from
 bodyParts = ['leftForepaw','rightForePaw','leftHindpaw','rightHindpaw','nose','tailBase']
 
 # Video directory variables
@@ -27,13 +25,29 @@ vidID = 'V' # Identifier for all wanted videos
 vidList = setDLCFunc.fileFinder(subjDir,vidID)
 
 ## Create cropping parameters for all video files
-[x1, x2, y1, y2] = setDLCFunc.getROI(vidList)
+[x1_all, x2_all, y1_all, y2_all] = setDLCFunc.getROI(vidList)
 
 ## Get randomly selected videos for training
 trainFiles = setDLCFunc.randVidSel(vidList,numRandVids)
 
 ## Create the DLC project!
 configPath = deeplabcut.create_new_project(projName,experimenter,trainFiles,working_directory=projDir,copy_videos=False)
+
+## Edit configuration file with bodyParts and training video crop parameters
+config = deeplabcut.utils.auxiliaryfunctions.read_config(configPath)
+
+config['bodyparts'] = bodyParts
+
+for vid in trainFiles:
+    paramIdx = vidList.index(vid)
+    params = [x1_all[paramIdx] + ', ' + x2_all[paramIdx] + ', ' + y1_all[paramIdx] + ', ' + y2_all[paramIdx]]
+    config['video_sets'][vid]['crop'] = params[0]
+    
+deeplabcut.utils.auxiliaryfunctions.write_config(configPath,config)
+
+## Extract frames for labeling
+deeplabcut.extract_frames(configPath,'automatic','uniform',crop=True,userfeedback=False)
+
 
 
 

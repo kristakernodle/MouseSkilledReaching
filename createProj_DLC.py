a1ce65cdbecf5d66e0d8ad9080d5e1c32df20b4b
kristakernodle <kristakernodle@gmail.com>
2019-06-05 15:14:44 -0400

    Added code to remove .png images (after extraction) that have the same size as the original video - our videos contain three views and something about DLC extraction causes one png per video to be extracted at the original size. Unknown why. This removes one file to avoid it.

--- createProj_DLC.py
+++ createProj_DLC.py
@@ -4,8 +4,10 @@ Created on Wed Jun  5 11:54:18 2019
 
 @author: kkrista
 """
+import os
 import setDLCFunc
 import deeplabcut
+from PIL import Image
 
 ## Define User Input Variables
 
@@ -13,8 +15,9 @@ import deeplabcut
 projName = 'left_grooming'
 experimenter = 'Krista'
 projDir = '/home/kkrista/Documents/Grooming/Krista/'
-numRandVids = 100 # number of videos to train from
+numRandVids = 15 # number of videos to train from
 bodyParts = ['leftForepaw','rightForePaw','leftHindpaw','rightHindpaw','nose','tailBase']
+vidDims = (1920, 1080)
 
 # Video directory variables
 subjDir = '/media/kkrista/KRISTAEHD/groomingDLC/' # Location of all animal files
@@ -48,6 +51,20 @@ deeplabcut.utils.auxiliaryfunctions.write_config(configPath,config)
 ## Extract frames for labeling
 deeplabcut.extract_frames(configPath,'automatic','uniform',crop=True,userfeedback=False)
 
+## Remove frames that are the same size as the original video
+projPath = config['project_path']
+pngList = setDLCFunc.fileFinder(projPath+'/labeled-data/','.png')
+for img in pngList:
+    im = Image.open(img)
+    size = im.size
+    
+    if size[0] >= vidDims[0] and size[1] >= vidDims[1]:
+        os.remove(img)
+    else:
+        continue
+    
+
+    
 
 
 

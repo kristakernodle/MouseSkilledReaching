fe06933b9cf6986c992450064e935e9c01155d39
kristakernodle <kristakernodle@gmail.com>
2019-06-05 13:44:22 -0400

    added fileFinder function. Finalized the getROI function for a vidList input

--- setDLCFunc.py
+++ setDLCFunc.py
@@ -5,6 +5,7 @@ Created on Thu Jan 24 15:57:44 2019
 
 @author: kkrista
 """
+import os
 import random
 import numpy as np
 import cv2
@@ -13,6 +14,19 @@ import cv2
 def readfile(F):
     with open(F) as f:
         return f.read().splitlines()
+    
+def fileFinder(mainDir,fileID):
+    
+    fileList = []
+    
+    for path, subdirs, files in os.walk(mainDir):
+        for name in files: 
+            if fileID in name:
+                fileList.append(path+'/'+name)
+            else:
+                continue
+    
+    return fileList
     
 def randVidSel(vidList,numVids):
     trainFiles = []
@@ -47,7 +61,14 @@ def getROI(vidList):
         
         # Look to see if we've already cropped this date
         if date in allDates:
+            
+            dateIdx = allDates.index(date)
+            x1_all.append(x1_all[dateIdx])
+            x2_all.append(x2_all[dateIdx])
+            y1_all.append(y1_all[dateIdx])
+            y2_all.append(y2_all[dateIdx])
             continue
+        
         else:
             vidcap = cv2.VideoCapture(vid)
             success, image = vidcap.read()
@@ -56,7 +77,9 @@ def getROI(vidList):
             r = cv2.selectROI(small)
             r = np.array(r)
             r = 2*r
-
+            
+            cv2.destroyAllWindows()
+            
             x1_all.append(str(r[0]))
             x2_all.append(str(r[0]+r[2]))
             y1_all.append(str(r[1]))

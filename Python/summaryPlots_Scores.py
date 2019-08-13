0a7380d885b909a1a4190c05cb33639d6650707d
kristakernodle <kristakernodle@gmail.com>
2019-08-13 13:46:31 -0400

    Almost done, but the plots aren't made

--- Python/summaryPlots_Scores.py
+++ Python/summaryPlots_Scores.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3.7
 
 import os
-import numpy as np
-import matplotlib.pyplot as plt
 
 animalDir = '/Volumes/SharedX/Neuro-Leventhal/data/mouseSkilledReaching/'
 
 # Initialize Variables
 allAnimals=[]
 allFolders = os.listdir(animalDir)
+needAbMov = []
+needGroom = []
 
 # Get all animal folders
 for file in allFolders:
@@ -36,6 +36,10 @@ for animal in allAnimals:
 
     for day in allTrainDays:
 
+        tday = day.split('_')[-1]
+        if len(tday) < 3:
+            tday = 'T0' + tday[-1]
+        
         if ('.MP4' in day):
             # Skip .MP4 files in 'Training' directory
             continue
@@ -52,88 +56,107 @@ for animal in allAnimals:
 
         # Separate out files and directories by type
         scoreFiles = [file for file in allFiles if file.endswith('Scored.csv')]
-#        vidFiles = [file for file in allFiles if file.endswith('.MP4')]
+        vidFiles = [file for file in allFiles if file.endswith('.MP4')]
         reachDirs = [file for file in allFiles if 'Reaches' in file]
         
-        if len(scoreFiles) < len(reachDirs):
-            # This means not all videos/reachDirs have been scored, continue to 
-            # the next training day
+        if len(reachDirs) < len(vidFiles) or (len(vidFiles) == 0 and len(reachDirs) == 0) or len(scoreFiles) < len(reachDirs):
+            # This means not all videos have had the pre-processing done, 
+            # continue to the next training day
+            tDayData = [tday,999,999,999,999,999]
+            summary.append(tDayData)
             continue
         
         reach = []
         abMov = []
         groom = []
         
+        skipAbMov = False
+        skipGroom = False
+        
         for file in scoreFiles:
             
+            # Open the file and save into scores
             with open(currDayDir + '/' + file) as f:
                 scores = f.read().splitlines()
                 scores = scores[1:]
                 
-            for trial in scores:
-                trial = trial.split(',')
-                    
-                if len(trial[1]) > 0:
-                    reach.append(int(trial[1]))
-                    needBreak = False
-                else:
-                    needBreak = True
-                    break
-                        
-                if len(trial[2]) > 0:
-                    abMov.append(int(trial[2]))
-                            
-                if len(trial[3]) > 0:
-                    groom.append(int(trial[3]))
+            for entry in scores:
+                entry = entry.split(',')
                 
-            if needBreak is True:
-                break
-        
-        if needBreak is True:
-            print('the day has not been completely scored')
+                if len(entry[1]) == 0:
+                    skipDay = True
+                    break
+                else:
+                    skipDay = False
+                    
+                    reach.append(int(entry[1]))
+                    
+                    if len(entry[2]) > 0 and skipAbMov == False:
+                        abMov.append(int(entry[2]))
+                    else:
+                        
+                        if skipAbMov == False:
+                            needAbMov.append(currDayDir)
+                            
+                        skipAbMov = True
+                        abMov = []
+                        
+                    if len(entry[3]) > 0 and skipGroom == False:
+                        groom.append(int(entry[3]))
+                    else:
+                        
+                        if skipGroom == False:
+                            needGroom.append(currDayDir)
+                        
+                        skipGroom = True
+                        groom = []         
+            
+        if skipDay == True:
+            tDayData = [tday,999,999,999,999,999]
+            summary.append(tDayData)
+            
             continue
-        
-        print('all data for this day is collected')
-        
+               
         score1 = reach.count(1)
         score2 = reach.count(2)
         
         firstSuc = score1 / len(reach) * 100
         anySuc = score2 / len(reach) * 100
-
-        totAbMov = sum(abMov)
-        percAbMov = totAbMov / len(reach) * 100
         
-        tday = day.split('_')[-1]
+        if len(abMov) != 0:
+            totAbMov = sum(abMov)
+            percAbMov = totAbMov / len(reach) * 100
+        else:
+            totAbMov = 999
+            percAbMov = 999
         
-        if len(tday) < 3:
-            tday = 'T0' + tday[-1]
+        if len(groom) != 0:
+            totGroom = sum(groom)
+        else:
+            totGroom = 999
         
-        tDayData = [tday,firstSuc,anySuc,totAbMov,percAbMov]
+        tDayData = [tday,firstSuc,anySuc,totAbMov,percAbMov,totGroom]
         
         summary.append(tDayData)
-        
-    print('This animal summary stats are collected')
     
     summary.sort()
+    f = open(currAnDir + 'summaryStatistics_' + animal + '.csv','w+')
+    f.write('Training Day,First Success,Any Success, Trials with Abnormal Movement,Percent Trials with Abnormal Movemovent, Trials with Grooming\n')
+    for tDay in summary:
+        f.write('%s,%f,%f,%f,%f,%f\n' %(tDay[0],tDay[1],tDay[2],tDay[3],tDay[4],tDay[5]))
+    f.close
     
+needAbMov.sort()
+f = open(animalDir + 'needAbMov.txt','w+')
+for item in needAbMov:
+    f.write('%s\n' %item)
+f.close
     
-    
-    xaxis = []
-    allFirstSuc = []
-    allAnySuc = []
-    allTotAbMov = []
-    allPercAbMov = []
-    
-    for day in summary:
-        xaxis.append(day[0])
-        allFirstSuc.append(day[1])
-        allAnySuc.append(day[2])
-        allTotAbMov.append(day[3])
-        allPercAbMov.append(day[4])
-        
-    plt.plot(xaxis,allFirstSuc)
-    
+needGroom.sort()
+f = open(animalDir + 'needGroom.txt','w+')
+for item in needGroom:
+    f.write('%s\n' %item)
+f.close
     
         
         

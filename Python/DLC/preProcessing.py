b53599aae8aa4c44a49a4940c32fee334fea2fdb
kristakernodle <kristakernodle@gmail.com>
2019-08-28 12:35:02 -0400

    Almost done formatting figures. Need to update frameDict for grooming and rest of abnormal movement

--- Python/DLC/preProcessing.py
+++ Python/DLC/preProcessing.py
@@ -18,6 +18,7 @@ Created on Fri Aug 16 13:08:38 2019
 import os
 import funcs
 import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator
 import numpy as np
 from scipy.interpolate import interp1d
 import frameDict
@@ -231,94 +232,164 @@ for animal in animals:
                 fdR = interp1d(dR_compressed,euclidDistRight_compressed,kind = 'cubic')
                 dfRnew = fdR(dRnew)
                 
-                xLScale.append(np.ma.max(xfLnew[frame1:frame2]) - np.ma.min(xfLnew[frame1:frame2]))
-                xRScale.append(np.ma.max(xfRnew[frame1:frame2]) - np.ma.min(xfRnew[frame1:frame2]))
-                
-#                yLScale.append(np.ma.max(yLeftPaw_masked[frame1:frame2]) - np.ma.min(yLeftPaw_masked[frame1:frame2]))
-#                yRScale.append(np.ma.max(yRightPaw_masked[frame1:frame2]) - np.ma.min(yRightPaw_masked[frame1:frame2]))
-#                
-#                eucLScale.append(np.ma.max(euclidDistLeft_masked[frame1:frame2]) - np.ma.min(euclidDistLeft_masked[frame1:frame2]))
-#                eucRScale.append(np.ma.max(euclidDistRight_masked[frame1:frame2]) - np.ma.min(euclidDistRight_masked[frame1:frame2]))
-#                
-
+                ## Creating y-axis limits
                 xLmin = np.floor(np.ma.min(xfLnew[frame1:frame2]))
                 xLmax = np.ceil(np.ma.max(xfLnew[frame1:frame2]))
                 xRmin = np.floor(np.ma.min(xfRnew[frame1:frame2]))
                 xRmax = np.ceil(np.ma.max(xfRnew[frame1:frame2]))
                 
-                Ldiff = xLmax - xLmin
-                Rdiff = xRmax - xRmin
+                yLmin = np.floor(np.ma.min(yfLnew[frame1:frame2]))
+                yLmax = np.ceil(np.ma.max(yfLnew[frame1:frame2]))
+                yRmin = np.floor(np.ma.min(yfRnew[frame1:frame2]))
+                yRmax = np.ceil(np.ma.max(yfRnew[frame1:frame2]))
                 
-                if Ldiff < Rdiff:
-                    while Rdiff < 350:
-                        xRmin += -1
-                        xRmax += 1
-                        Rdiff = xRmax - xRmin
-                        
-                    xMin = xRmin
-                    xMax = xRmax
+                dfLmin = np.floor(np.ma.min(dfLnew[frame1:frame2]))
+                dfLmax = np.ceil(np.ma.max(dfLnew[frame1:frame2]))
+                dfRmin = np.floor(np.ma.min(dfRnew[frame1:frame2]))
+                dfRmax = np.ceil(np.ma.max(dfRnew[frame1:frame2]))
+                
+                xLdiff = xLmax - xLmin
+                xRdiff = xRmax - xRmin
+                
+                yLdiff = yLmax - yLmin
+                yRdiff = yRmax - yRmin
+                
+                dLdiff = dfLmax - dfLmin
+                dRdiff = dfRmax - dfRmin
+                
+                xWin = 350
+                yWin = 450
+                dWin = 450
+                
+                while xRdiff < xWin:
+                    xRmin += -1
+                    xRmax += 1
+                    xRdiff = xRmax - xRmin
+
+                while xLdiff < xWin:
+                    xLmin += -1
+                    xLmax += 1
+                    xLdiff = xLmax - xLmin
+                    
+                while yRdiff < yWin:
+                    yRmin += -1
+                    yRmax += 1
+                    yRdiff = yRmax - yRmin
+
+                while xLdiff < xWin:
+                    yLmin += -1
+                    yLmax += 1
+                    yLdiff = yLmax - yLmin
+                    
+                while dRdiff < dWin:
+                    dfRmin += -1
+                    dfRmax += 1
+                    dRdiff = dfRmax - dfRmin
+
+                while dLdiff < dWin:
+                    dfLmin += -1
+                    dfLmax += 1
+                    dLdiff = dfLmax - dfLmin
+
+                if dfLmin < dfRmin:
+                    dfMin = dfLmin
                 else:
-                    while Ldiff < 250:
-                        xLmin += -1
-                        xLmax += 1
-                        Ldiff = xLmax - xLmin
-                    xMin = xLmin
-                    xMax= xLmax
-#                xMin = 100
-#                xMax = 900
-                yMin = 0
-                yMax = 900
-                eucMin = -550
-                eucMax = 550
+                    dfMin = dfRmin
                 
+                if dfLmax < dfRmax:
+                    dfMax = dfRmax
+                else:
+                    dfMax = dfLmax
+                    
                 ## Begin Plotting
                 
                 fig = plt.figure()
-                ax1 = fig.add_subplot(231)
-                ax2 = fig.add_subplot(232)
-                ax5 = fig.add_subplot(233)
-                ax3 = fig.add_subplot(234)
-                ax4 = fig.add_subplot(235)
-                ax6 = fig.add_subplot(236)
+                ax1 = fig.add_subplot(321)
+                ax2 = fig.add_subplot(322)
+                ax3 = fig.add_subplot(325)
                 
+                ax4 = fig.add_subplot(323)
+                ax5 = fig.add_subplot(324)
+                ax6 = fig.add_subplot(326)
+                
+                
+                
+                ## LEFT PAW - First Row - Subplots 1-3
                             
+                # Left Paw x-values
                 ax1.plot(x[frame1:frame2],xLeftPaw_masked[frame1:frame2],'o',label='Original Data')
                 ax1.plot(xLnew[frame1:frame2],xfLnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
                 ax1.axvspan(oframe1/fps, oframe2/fps, facecolor='#d7f4d7', alpha=0.5)
-                ax1.set_ylim([xMin,xMax])
-
+                ax1.set_ylim([xLmin,xLmax])
+                ax1.yaxis.set_ticks(np.arange(xLmin,xLmax,100))
+                ax1.tick_params(axis='both', which='both', labelsize=5)
+                ax1.yaxis.set_major_locator(LinearLocator(4))
+                ax1.xaxis.set_major_locator(LinearLocator(5))
+                ax1.set_title('Left Paw, x-values', size = 8)
+                ax1.set_xticklabels([])
             
-                ax2.plot(x[frame1:frame2],xRightPaw_masked[frame1:frame2],'o',label='Original Data')
-                ax2.plot(xRnew[frame1:frame2],xfRnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
+                # Left Paw y-values
+                ax2.plot(y[frame1:frame2],yLeftPaw_masked[frame1:frame2],'o',label='Original Data')
+                ax2.plot(yLnew[frame1:frame2],yfLnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
                 ax2.axvspan(oframe1/fps, oframe2/fps, facecolor='#d7f4d7', alpha=0.5)
-                ax2.set_ylim([100,500])
-
+                ax2.set_ylim([yLmin,yLmax])
+                ax2.yaxis.set_ticks(np.arange(yLmin,yLmax,100))
+                ax2.tick_params(axis='both', which='both', labelsize=5)
+                ax2.yaxis.set_major_locator(LinearLocator(5))
+                ax2.xaxis.set_major_locator(LinearLocator(5))
+                ax2.set_title('Left Paw, y-values', size = 8)
+                ax2.set_xticklabels([])
                 
-                ax3.plot(y[frame1:frame2],yLeftPaw_masked[frame1:frame2],'o',label='Original Data')
-                ax3.plot(yLnew[frame1:frame2],yfLnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
-                ax3.axvspan(oframe1/fps, oframe2/fps, facecolor='#d7f4d7', alpha=0.5)
-                ax3.set_ylim([xMin,xMax])
-
+                # Left Paw Euclidean Distance From Nose
+                ax3.plot(xD[frame1:frame2],euclidDistLeft_masked[frame1:frame2],'o',label='Original Data')
+                ax3.plot(dLnew[frame1:frame2],dfLnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
+                ax3.axvspan(oframe1/fps,oframe2/fps,facecolor='#d7f4d7', alpha=0.5)
+                ax3.set_ylim([dfLmin,dfLmax])
+                ax3.yaxis.set_ticks(np.arange(dfMin,dfMax,100))
+                ax3.tick_params(axis='both', which='both', labelsize=5)
+                ax3.yaxis.set_major_locator(LinearLocator(5))
+                ax3.xaxis.set_major_locator(LinearLocator(5))
+                ax3.set_title('Left Paw, ED from nose', size = 8)
                 
-                ax4.plot(y[frame1:frame2],yRightPaw_masked[frame1:frame2],'o',label='Original Data')
-                ax4.plot(yRnew[frame1:frame2],yfRnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
+                # Right Paw x-values
+                ax4.plot(x[frame1:frame2],xRightPaw_masked[frame1:frame2],'o',label='Original Data')
+                ax4.plot(xRnew[frame1:frame2],xfRnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
                 ax4.axvspan(oframe1/fps, oframe2/fps, facecolor='#d7f4d7', alpha=0.5)
-                ax4.set_ylim([yMin,yMax])
+                ax4.set_ylim([xRmin,xRmax])
+                ax4.yaxis.set_ticks(np.arange(xRmin,xRmax,100))
+                ax4.tick_params(axis='both', which='both', labelsize=5)
+                ax4.yaxis.set_major_locator(LinearLocator(4))
+                ax4.xaxis.set_major_locator(LinearLocator(5))
+                ax4.set_title('Right Paw, x-values', size = 8)
+                ax4.set_xticklabels([])
+                
+                # Right Paw y-values
+                ax5.plot(y[frame1:frame2],yRightPaw_masked[frame1:frame2],'o',label='Original Data')
+                ax5.plot(yRnew[frame1:frame2],yfRnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
+                ax5.axvspan(oframe1/fps, oframe2/fps, facecolor='#d7f4d7', alpha=0.5)
+                ax5.set_ylim([yRmin,yRmax])
+                ax5.yaxis.set_ticks(np.arange(yRmin,yRmax,100))
+                ax5.tick_params(axis='both', which='both', labelsize=5)
+                ax5.yaxis.set_major_locator(LinearLocator(5))
+                ax5.xaxis.set_major_locator(LinearLocator(5))
+                ax5.set_title('Right Paw, y-values', size = 8)
+                ax5.set_xticklabels([])
 
-                
-                ax5.plot(xD[frame1:frame2],euclidDistLeft_masked[frame1:frame2],'o',label='Original Data')
-                ax5.plot(dLnew[frame1:frame2],dfLnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
-                ax5.axvspan(oframe1/fps,oframe2/fps,facecolor='#d7f4d7', alpha=0.5)
-                ax5.set_ylim([eucMin,eucMax]) 
- 
-                
+                # Right Paw Euclidean Distance From Nose
                 ax6.plot(xD[frame1:frame2],euclidDistRight_masked[frame1:frame2],'o',label='Original Data')
                 ax6.plot(dRnew[frame1:frame2],dfRnew[frame1:frame2],'-',label='Cubic 1D Interpolation')
                 ax6.axvspan(oframe1/fps,oframe2/fps,facecolor='#d7f4d7', alpha=0.5) 
-                ax6.set_ylim([eucMin,eucMax])
+                ax6.set_ylim([dfRmin,dfRmax])
+                ax6.yaxis.set_ticks(np.arange(dfMin,dfMax,100))
+                ax6.tick_params(axis='both', which='both', labelsize=5)
+                ax6.yaxis.set_major_locator(LinearLocator(5))
+                ax6.xaxis.set_major_locator(LinearLocator(5))
+                ax6.set_title('Right Paw, ED from nose', size = 8)
 
                 
+                fig.suptitle(folder + ', Video: ' + reach + ' Abnormal Movement', size=10)
                 fig.tight_layout()
+                fig.subplots_adjust(top=0.88)
                 fig.savefig(dirDLC + 'DLCProcessing/' + file[:-3] + 'pdf')
 
                 plt.close()

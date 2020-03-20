d41736b841dbee3da6e31d21029b84e50d4e5dbf
Kristas-MacBook-Pro <kristakernodle@gmail.com>
2020-03-19 22:22:38 -0400

    Creates dictionary of scored files with filenames as keys and scorers as values

--- Python/summaryFiles/animalInfo.py
+++ Python/summaryFiles/animalInfo.py
@@ -6,34 +6,34 @@ Created on Mon Jul  8 16:09:01 2019
 @author: kkrista
 """
 
-#animals = {
-#        "et704":    ['WT','left', 'box2', '.'],
-#        "et710":    ['KO','right','box1','.'],
-#        "et713":    ['WT','right','box1',','],
-#        "et717":    ['KO','right','box2',','],
-#        "et719":    ['KO','right','box1','o'],
-#        "et740":    ['WT','left', 'box2','o'],
-#        "et743":    ['WT','left', 'box2','^'],
-#        "et745":    ['KO','left', 'box1','^'],
-#        "et749":    ['WT','left', 'box1','<'],
-#        "et757":    ['WT','left', 'box1','>'],
-#        "et764":    ['WT','right','box1','1'],
-#        "et7061":   ['KO','left', 'box1','<'],
-#        "et7062":   ['KO','right','box2','>'],
-#        "et7063":   ['WT','right','box2','2'],
-#        "et7064":   ['WT','left', 'box1','3'],
-#        "et7065":   ['WT','left', 'box2','4'],
-#        "et7068":   ['WT','left', 'box1','8'],
-#        "et7076":   ['KO','left', 'box1','2'],
-#        "et7081":   ['WT','999','box2','s']
-#        }
-
-## Lawrence Lab Collaboration (added 1/17/2020)
 animals = {
-        "et7126": ['exp','right','box1','.'],
-        "et7127": ['sham','left','box2',','],
-        "et7128": ['exp','left','box1','o'],
-        "et7129": ['exp','left','box2','^'],
-        "et7131": ['exp','right','box2','<'],
-        "et7132": ['sham','right','box1','>']
-        }
+       "et704":    ['WT','left', 'box2', '.'],
+       "et710":    ['KO','right','box1','.'],
+       "et713":    ['WT','right','box1',','],
+       "et717":    ['KO','right','box2',','],
+       "et719":    ['KO','right','box1','o'],
+       "et740":    ['WT','left', 'box2','o'],
+       "et743":    ['WT','left', 'box2','^'],
+       "et745":    ['KO','left', 'box1','^'],
+       "et749":    ['WT','left', 'box1','<'],
+       "et757":    ['WT','left', 'box1','>'],
+       "et764":    ['WT','right','box1','1'],
+       "et7061":   ['KO','left', 'box1','<'],
+       "et7062":   ['KO','right','box2','>'],
+       "et7063":   ['WT','right','box2','2'],
+       "et7064":   ['WT','left', 'box1','3'],
+       "et7065":   ['WT','left', 'box2','4'],
+       "et7068":   ['WT','left', 'box1','8'],
+       "et7076":   ['KO','left', 'box1','2'],
+       "et7081":   ['WT','999','box2','s']
+       }
+
+# ## Lawrence Lab Collaboration (added 1/17/2020)
+# animals = {
+#         "et7126": ['exp','right','box1','.'],
+#         "et7127": ['sham','left','box2',','],
+#         "et7128": ['exp','left','box1','o'],
+#         "et7129": ['exp','left','box2','^'],
+#         "et7131": ['exp','right','box2','<'],
+#         "et7132": ['sham','right','box1','>']
+#         }
